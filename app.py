20f194ffb43b0f84dfe253a88430df3693e065ad
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2019-04-24 22:00:37 +0700

    Add send request every x seconds to keep heroku alive

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+import os
+import multiprocessing as mp
+import sys
 from os import environ
 
 from flask import Flask
 
+from utils.http_request.send import SendHTTPRequest
+from utils.log.log import ErrorLog, InfoLog
+
+module_name = "app.py"
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,4 +16,15 @@ def hello():
 	return "Hello World!"
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',debug=False, port=environ.get("PORT", 5000))
+	try:
+		InfoLog(module_name, "START SERVER")
+		multi_process = mp.Process(target=SendHTTPRequest)
+		multi_process.start()
+		app.run(host='0.0.0.0',debug=False, port=environ.get("PORT", 5000))
+		multi_process.join()
+	except Exception as e :
+		multi_process.close()
+		ErrorLog(module_name, e)
+		InfoLog(module_name, "END SERVER")
+		os.execv(sys.executable, [sys.executable, 'app.py'] + sys.argv)
+		sys.exit()
---
+++ utils/http_request/send.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import time
+import urllib.request
+
+from utils.log.log import ErrorLog, InfoLog
+
+module_name = "utils.log.log.py"
+
+def SendHTTPRequest():
+	InfoLog(module_name, "START SEND REQUEST")
+	while True :
+		try :
+			time.sleep(1740)
+			contents = urllib.request.urlopen("http://dashboard-angkot-to-school.herokuapp.com/").read()
+			InfoLog(module_name, contents)
+		except Exception as e :
+			ErrorLog(module_name, e)
+			InfoLog(module_name, "END SEND REQUEST")
+			sys.exit()
