bf3d9701f0c90040c1b9cd4c379f0b02191f0b0a
handikao29 <handikaoktavianus29@gmail.com>
2019-05-06 11:46:15 +0700

    Added bar chart, changed body header

--- static/javascript/chart.js
+++ static/javascript/chart.js
@@ -1,6 +1,6 @@
 // Load google charts
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawMultSeries);
 
 // Draw the chart and set the chart values
 function drawChart() {
@@ -20,4 +20,28 @@ function drawChart() {
     // Display the chart inside the <div> element with id="piechart"
     var chart = new google.visualization.PieChart(document.getElementById('piechart'));
     chart.draw(data, options);
+
+    var databar = google.visualization.arrayToDataTable([
+        ['Kecamatan', 'Banyak angkot', 'Banyak angkot yang dibutuhkan'],
+        ['New York City, NY', 8175000, 8008000],
+        ['Los Angeles, CA', 3792000, 3694000],
+        ['Chicago, IL', 2695000, 2896000],
+        ['Houston, TX', 2099000, 1953000],
+        ['Philadelphia, PA', 1526000, 1517000]
+      ]);
+
+    var optionsbar = {
+    'title': '5 Kecamatan yang Paling Kekurangan Angkot',
+    'chartArea': {'width': '50%'},
+    'hAxis': {
+       'title': 'Total Population',
+        'minValue': 0
+    },
+    'vAxis': {
+        'title': 'City'
+    }
+    };
+
+    var chartbar = new google.visualization.BarChart(document.getElementById('kecamatan'));
+    chartbar.draw(databar, optionsbar);
 }
